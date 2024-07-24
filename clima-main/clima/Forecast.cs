2952cc42ae5b7bf2f799137dbf428a46bf90e23f
alastorhel <146208738+alastorhel@users.noreply.github.com>
2024-07-24 07:54:28 -0300

    Add files via upload

---
+++ clima-main/clima/Forecast.cs
@@ -0,0 +1,14 @@
+namespace clima;
+ 
+ public class Forcast
+ {
+    public string weekday  {get;set;}
+    public float max {get;set;}
+    public float min {get;set;}
+    public float Cloudiness {get;set;}
+    public float Rain {get;set;}
+    public float RainProbability {get;set;}
+    public string WindSpeed {get;set;}
+    public string description {get;set;}
+ }
+
---
+++ clima-main/clima/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.Json;
+
+namespace clima;
+
+public partial class MainPage : ContentPage
+{
+	const string Url="https://api.hgbrasil.com/weather?woeid=455927&key=54f7cbee";
+	Resposta resposta;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		AtualizaTempo();
+		
+	}
+
+		
+
+	void PreencherTela()
+	{
+		LabelChuva.Text = resposta.results.rain.ToString();
+		LabelHumidade.Text = resposta.results.humidity.ToString();
+		LabelGraus.Text = resposta.results.temp.ToString();
+		LabelClima.Text = resposta.results.description;
+		LabelCidade.Text = resposta.results.city;
+		LabelForça.Text = resposta.results. wind_speed.ToString();
+		LabelDireção.Text = resposta.results.wind_direction.ToString();
+		LabelAmanhecer.Text = resposta.results.sunrise;
+		LabelAnoitecer.Text = resposta.results.sunset;
+		LabelLua.Text = resposta.results.moon_phase;
+		LabelCardinal.Text = resposta.results.wind_cardinal;
+		LabelNublado.Text = resposta.results.cloudness.ToString();
+
+		if (resposta.results.currently == "dia")
+		{
+			if (resposta.results.rain >= 35)
+				imgBackground.Source = "diachuva.webp";
+			else if (resposta.results.cloudness >= 35)
+				imgBackground.Source = "dianublado.webp";
+			else
+				imgBackground.Source = "dia.webp";
+		}
+		else if (resposta.results.currently == "noite")
+		{
+			if (resposta.results.rain >= 35)
+				imgBackground.Source = "noitechuva.webp";
+			else if (resposta.results.cloudness >= 35)
+				imgBackground.Source = "noitenublada.webp";
+			else
+				imgBackground.Source = "noite.webp";
+		}
+
+	}
+
+	async void AtualizaTempo()
+	{
+		try
+		{
+			var httpClient = new HttpClient();
+			var response  = await httpClient.GetAsync(Url);
+			if (response.IsSuccessStatusCode)
+			{
+				string content = await response.Content.ReadAsStringAsync();
+				resposta = JsonSerializer.Deserialize<Resposta>(content);
+			}
+		}
+		catch(Exception e)
+		{
+			//Erro
+			
+		}
+
+		PreencherTela();
+	}
+		
+}
+
